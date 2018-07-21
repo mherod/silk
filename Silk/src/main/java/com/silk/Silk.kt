4e2c62f3d5039f11be9ecb0c2654db56e0fbdcf3
Graham Smith <graham@wiseman-designs.com>
2018-07-21 11:22:29 +0100

    Added more prototype methods.

--- Silk/src/main/java/com/silk/Silk.kt
+++ Silk/src/main/java/com/silk/Silk.kt
@@ -2,12 +2,25 @@ package com.silk
 
 import android.webkit.WebView
 
+typealias WebViewTitleChangeListener = (title: String) -> Unit
+
 class Silk (
         val webView: WebView,
-        val urlInterceptor: List<UrlChangeInterceptor>?
+        val urlInterceptor: List<UrlChangeInterceptor>?,
+        val titleChangeListeners: List<WebViewTitleChangeListener>?,
+        val headers: HashMap<String, String>,
+        val enableJavascript: Boolean,
+        val enableJavascriptToOpenNewWindows: Boolean,
+        val isDebuggable: Boolean
 ) {
 
-    private constructor(builder: Builder) : this(builder.webView, builder.urlInterceptors)
+    private constructor(builder: Builder) : this(builder.webView,
+            builder.urlInterceptors,
+            builder.titleChangeListeners,
+            builder.headers,
+            builder.enableJavascript,
+            builder.enableJavascriptToOpenNewWindows,
+            builder.isDebuggable)
 
     companion object {
         inline fun createSilk(webView: WebView, block: Builder.() -> Unit) = Builder(webView).apply(block).build()
@@ -16,9 +29,35 @@ class Silk (
     class Builder(val webView: WebView) {
 
         var urlInterceptors = mutableListOf<UrlChangeInterceptor>()
+        var titleChangeListeners = mutableListOf<WebViewTitleChangeListener>()
+        var headers = hashMapOf<String, String>()
+        var enableJavascript = false
+        var enableJavascriptToOpenNewWindows = false
+
+        var isDebuggable = false
 
         fun addUrlChangeInterceptor(urlChangeInterceptor: UrlChangeInterceptor) {
             urlInterceptors.add(urlChangeInterceptor)
+        }
+
+        fun addTitleChangeListener(titleChangeListener: WebViewTitleChangeListener) {
+            titleChangeListeners.add(titleChangeListener)
+        }
+
+        fun addHeadersToRequests(headers: HashMap<String, String>) {
+            this.headers.putAll(headers)
+        }
+
+        fun enableJavascript() {
+            enableJavascript = true
+        }
+
+        fun enableJavascriptToOpenNewWindows() {
+            enableJavascriptToOpenNewWindows = true
+        }
+
+        fun enableDebugging(isDebuggable: Boolean) {
+            this.isDebuggable = isDebuggable
         }
 
         fun build() = Silk(this)
--- app/src/main/java/com/silk/sample/MainActivity.kt
+++ app/src/main/java/com/silk/sample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.silk.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.sample.BuildConfig
 import com.sample.R
 import com.silk.Silk.Companion.createSilk
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,6 +19,27 @@ class MainActivity : AppCompatActivity() {
             addUrlChangeInterceptor(null)
             addUrlChangeInterceptor(null)
 
+            addTitleChangeListener(::handleTitleChange)
+
+            addHeadersToRequests(provideExtraHeaders())
+
+            enableJavascript()
+            enableJavascriptToOpenNewWindows()
+
+            enableDebugging(isDebugabble())
+
         }
+    }
+
+    private fun handleTitleChange(title: String) {
+        Toast.makeText(this, title, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun provideExtraHeaders(): HashMap<String, String> {
+        return hashMapOf("" to "")
+    }
+
+    private fun isDebugabble(): Boolean {
+        return BuildConfig.DEBUG
     }
 }
